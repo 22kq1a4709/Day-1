0bfbe244a110620fb02da5192894deccb06a8211
22kq1a4709 <22kq1a4709@pace.ac.in>
2025-08-15 21:28:12 +0530

    Update count frequency in string.py

--- count frequency in string.py
+++ count frequency in string.py
@@ -4,3 +4,14 @@ for char in text:
     if text.count(char) > 0:
         print(char, ":", text.count(char))
         text = text.replace(char, "")
+
+
+
+#sample output
+Enter a string: ramsita
+r : 1
+a : 2
+m : 1
+s : 1
+i : 1
+t : 1
